d9b164d534a0724b2214724799f149fa580a8cd7
Christian Wierzbicki <cwierzbicki@townchest.com>
2023-07-18 21:45:30 -0400

    fix: conversion script csv -> json

--- csv_to_json.py
+++ csv_to_json.py
@@ -1,14 +1,41 @@
-import pandas as pd
+import csv
+import json
 
-#Replace 'input.csv' with the path to your CSV file.
-input_csv = 'input.csv'
 
-#Replace 'output.json' with the desired path for your JSON file.
-output_json = 'output.json'
+def write_json(data, filename):
+    with open(filename, 'w') as f:
+        json.dump(data, f, indent=4)
 
-#Read the CSV file into a pandas DataFrame
-data_frame = pd.read_csv(input_csv)
 
-#convert the DataFrame to a JSON object and save it to a file.
-data_frame.to_json(output_json, orient='records' , lines=True)
+def csv_to_json(csvFilePath):
+    data1 = []
+    data2 = []
+    switch = False
 
+    player_fields = ["DATASET", "GAME-ID", "DATE", "PLAYER-ID", "PLAYER FULL NAME", "POSITION", "OWN TEAM",
+                     "OPPONENT TEAM", "FG", "FGA", "3P", "3PA", "FT", "FTA", "OR", "DR", "TOT", "A", "PF", "ST", "TO", "BL", "PTS"]
+    team_fields = ["INITIALS", "LONG NAME",
+                   "SHORT NAME", "CONFERENCE", "DIVISION"]
+
+    with open(csvFilePath, encoding='utf-8') as csvf:
+        csvReader = csv.reader(csvf)
+
+        for row in csvReader:
+            if row[0] == "NBA-2022-23-PLAYER: Table 1":
+                continue
+            elif row[0] == "TEAMS: Table 1":
+                switch = True
+                continue
+            if not any(row):
+                continue
+            if switch:
+                data2.append(dict(zip(team_fields, row)))
+            else:
+                data1.append(dict(zip(player_fields, row)))
+
+    write_json(data1, 'game_stats.json')
+    write_json(data2, 'teams.json')
+
+
+csvFilePath = 'input.csv'
+csv_to_json(csvFilePath)
